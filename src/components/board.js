06c53c19139c4d1d2287ec6949795a5e66932219
Dominik Mikes <dominik.blau@gmail.com>
2023-02-08 09:59:12 +0100

    fixed some small bugs

---
+++ src/components/board.js
@@ -0,0 +1,81 @@
+import { createContext, useCallback, useLayoutEffect, useReducer, useState } from 'react';
+import './board.css';
+import Cell from './cell';
+
+const rows = 3;
+export const PlayerContext = createContext();
+
+function fillCells(state, action) {
+    state[action.value] = action.type;
+    return state;
+}
+
+function checkWinner(cells) {
+    if (cells.length >= 5) {
+        const combs = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6] 
+        ];
+
+        for (let comb of combs) {
+            if (
+                cells[comb[0]] === cells[comb[1]] 
+                    && 
+                cells[comb[1]] === cells[comb[2]] 
+                    && 
+                cells[comb[0]] !== ''
+            ) {
+                return cells[comb[0]];
+            }
+        }
+    }
+    return false;
+}
+
+export default function Board() {
+    const [player, setPlayer] = useState('X');
+    const [cells, setCells] = useReducer(fillCells, []);
+    const [winner, setWinner] = useState(false);
+
+    useLayoutEffect(() => {
+        setWinner(checkWinner(cells));
+    }, [cells, player]);
+    
+    function renderRows() {
+        let rowElements = [];
+        let cellCnt = 0;
+        for (let i=0; i < rows; i++) {
+            rowElements.push(<div key={'row' + i} className="board-row">
+                                <Cell key={i + 0} playerSymbol={cells[cellCnt]} value={cellCnt++} setCells={setCells}></Cell>
+                                <Cell key={i + 1} playerSymbol={cells[cellCnt]} value={cellCnt++} setCells={setCells}></Cell>
+                                <Cell key={i + 2} playerSymbol={cells[cellCnt]} value={cellCnt++} setCells={setCells}></Cell>
+                            </div>);
+        }
+
+        return rowElements;
+    }
+
+    const resetGame = useCallback(() => {
+        setCells([]);
+        setWinner(false);
+    }, [setWinner]);
+
+    return(
+        <>
+            <PlayerContext.Provider value={{player, setPlayer}}>
+                {renderRows()}
+                {winner &&
+                <div>
+                    Winner is {winner}. <button onClick={resetGame}>Reset</button>
+                </div>
+                }
+            </PlayerContext.Provider>
+        </>
+    )
+}
